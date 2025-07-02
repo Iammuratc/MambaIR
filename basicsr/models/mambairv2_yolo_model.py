f17cddb09b026e6c7f35eddaa3079aad90bcabcf
louis <louis.niederloehner@gmail.com>
2025-07-02 12:02:55 +0200

    Fix validation for Mamaba+Yolo

--- basicsr/models/mambairv2_yolo_model.py
+++ basicsr/models/mambairv2_yolo_model.py
@@ -1,13 +1,23 @@
+import os
+
 import torch
 from collections import OrderedDict
-from torch.nn import functional as F
 
-from basicsr import build_network
+from torch import nn
+from torch.nn import functional as F, DataParallel
+from torch.nn.parallel import DistributedDataParallel
+from tqdm import tqdm
+
+from basicsr.utils import imwrite, tensor2img
+from basicsr.archs import build_network
+from basicsr.metrics import calculate_metric
 from basicsr.utils.registry import MODEL_REGISTRY
 from basicsr.models.sr_model import SRModel
 from yolov12.ultralytics.models import YOLO
 from yolov12.ultralytics.utils.loss import v8DetectionLoss
 from yolov12.ultralytics.utils import IterableSimpleNamespace
+
+from os import path as osp
 
 
 @MODEL_REGISTRY.register()
@@ -204,3 +214,127 @@ class MambaIRv2YoloModel(SRModel):
         optim_type = train_opt['optim_g'].pop('type')
         self.optimizer_g = self.get_optimizer(optim_type, optim_params, **train_opt['optim_g'])
         self.optimizers.append(self.optimizer_g)
+
+
+    def dist_validation(self, dataloader, current_iter, tb_logger, save_img):
+        if self.opt['rank'] == 0:
+            self.nondist_validation(dataloader, current_iter, tb_logger, save_img)
+
+    def nondist_validation(self, dataloader, current_iter, tb_logger, save_img):
+        print(f"Save image path: {self.opt['path']['visualization']}")
+
+        dataset_name = dataloader.dataset.opt['name']
+        with_metrics = self.opt['val'].get('metrics') is not None
+        use_pbar = self.opt['val'].get('pbar', False)
+
+        if with_metrics:
+            if not hasattr(self, 'metric_results'):  # only execute in the first run
+                self.metric_results = {metric: 0 for metric in self.opt['val']['metrics'].keys()}
+            # initialize the best metric results for each dataset_name (supporting multiple validation datasets)
+            self._initialize_best_metric_results(dataset_name)
+        # zero self.metric_results
+        if with_metrics:
+            self.metric_results = {metric: 0 for metric in self.metric_results}
+
+        metric_data = dict()
+        if use_pbar:
+            pbar = tqdm(total=len(dataloader), unit='image')
+
+        for idx, val_data in enumerate(dataloader):
+            img_name = osp.splitext(osp.basename(val_data['lq_path'][0]))[0]
+            self.feed_data(val_data)
+            self.test()
+
+            visuals = self.get_current_visuals()
+            sr_img = tensor2img([visuals['result']])
+            metric_data['img'] = sr_img
+            if 'gt' in visuals:
+                gt_img = tensor2img([visuals['gt']])
+                metric_data['img2'] = gt_img
+                del self.gt
+
+            # tentative for out of GPU memory
+            del self.lq
+            del self.output
+            torch.cuda.empty_cache()
+
+            if save_img:
+                if self.opt['is_train']:
+                    save_img_path = osp.join(self.opt['path']['visualization'], img_name,
+                                             f'{img_name}_{current_iter}.png')
+                else:
+                    if self.opt['val']['suffix']:
+                        save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
+                                                 f'{img_name}_{self.opt["val"]["suffix"]}.png')
+                    else:
+                        save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
+                                                 f'{img_name}_{self.opt["name"]}.png')
+                imwrite(sr_img, save_img_path)
+
+            if with_metrics:
+                # calculate metrics
+                for name, opt_ in self.opt['val']['metrics'].items():
+                    self.metric_results[name] += calculate_metric(metric_data, opt_)
+            if use_pbar:
+                pbar.update(1)
+                pbar.set_description(f'Test {img_name}')
+        if use_pbar:
+            pbar.close()
+
+        if with_metrics:
+            for metric in self.metric_results.keys():
+                self.metric_results[metric] /= (idx + 1)
+                # update the best metric result
+                self._update_best_metric_result(dataset_name, metric, self.metric_results[metric], current_iter)
+
+            self._log_validation_metric_values(current_iter, dataset_name, tb_logger)
+
+    def save(self, epoch, current_iter):
+        if current_iter == -1:
+            current_iter = 'latest'
+        # Save SR model
+        save_filename = f'net_g_{current_iter}.pth'
+        sr_save_path = os.path.join(self.opt['path']['models'], save_filename)
+
+        print(f"SR model save path: {sr_save_path}")
+
+        if isinstance(self.net_g, (DataParallel, DistributedDataParallel)):
+            sr_net = self.net_g.module
+        sr_net_state_dict = sr_net.state_dict()
+        for key, param in sr_net_state_dict.items():
+            if key.startswith('module.'):  # remove unnecessary 'module.'
+                key = key[7:]
+            sr_net_state_dict[key] = param.cpu()
+
+        save_dict = {}
+        save_dict['params'] = sr_net_state_dict
+        torch.save(save_dict, sr_save_path)
+
+        # Save Yolo model
+
+        save_filename = f'yolo_{current_iter}.pth'
+        yolo_save_path = os.path.join(self.opt['path']['models'], save_filename)
+
+        from copy import deepcopy
+        from yolov12.ultralytics import __version__
+        from datetime import datetime
+
+        updates = {
+            "model": deepcopy(self.yolo_model).half() if isinstance(self.yolo_model, nn.Module) else self.yolo_model,
+            "date": datetime.now().isoformat(),
+            "version": __version__,
+            "license": "AGPL-3.0 License (https://ultralytics.com/license)",
+            "docs": "https://docs.ultralytics.com",
+        }
+        torch.save({**self.yolo_model.ckpt, **updates}, yolo_save_path)
+
+        print(f"Yolo model save path: {yolo_save_path}")
+
+
+
+
+        if hasattr(self, 'net_g_ema'):
+            self.save_network([self.net_g, self.net_g_ema], 'net_g', current_iter, param_key=['params', 'params_ema'])
+        else:
+            self.save_network(self.net_g, 'net_g', current_iter)
+        self.save_training_state(epoch, current_iter)
